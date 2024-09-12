bf75ec4410d0ce17b1afa1c900788392637402cf
Leonardo <leopedrassani201@gmail.com>
2024-09-11 22:04:34 -0300

    API e Post

--- primeira_api/index.js
+++ primeira_api/index.js
@@ -1,17 +1,44 @@
 const express = require("express");
+const bodyParser = require("body-parser");
+
 
 const app = express();
 
-app.get("/", (request, response)=>{
-    try {
-        throw new Error("Fodeu!");
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.get("/", (request, response) => {
         response.status(200).send("O seu servidor esta funcionando");
-    }
-    catch(error){
-        response.status(500).send("Erro inesperado!", error);
-    }
 });
 
+app.get("/novaRota", (request,response) => {
+    response.send("Essa é a nova rota!");
+});
+
+app.get("/clientes", (request,response)=>{
+
+    const clientes = [
+        {
+            nome: "Leonardo",
+            idade: 19
+        },
+        {
+            nome: "Espiriguidiberto",
+            idade: 2467
+        }
+    ]
+
+
+
+    response.send(clientes);
+})
+
+app.post("/cliente", (request, response) =>{
+
+    const cliente = request.body;
+
+    response.send(cliente);
+});
 
 app.listen(8080, () => {
     console.log("O servidor esta rodando na porta 8080!");
