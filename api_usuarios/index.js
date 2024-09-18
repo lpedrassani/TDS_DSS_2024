ae8666f2ace55684f38632a17719521c8076ce11
Leonardo <leopedrassani201@gmail.com>
2024-09-17 22:05:06 -0300

    Continuacao

--- api_usuarios/index.js
+++ api_usuarios/index.js
@@ -8,19 +8,19 @@ app.use(bodyParser.urlencoded({extended: false}));
 
 const clientes = [];
 
+
+
 app.post("/cadastro", (request,response)=>{
-
-    console.log(request.body);
-    clientes.push(request.body);
-    response.status(200).send("Usuario cadastrado com sucesso!");
-
+        
+        clientes.push(request.body);
+        console.log(clientes);
+        response.status(200).send("Usuario cadastrado com sucesso!");
 });
 
 app.get("/consulta", (request, response) =>{
-
     response.status(200).send(clientes);
 });
 
-app.listen(8080, () => {
+app.listen(8080, (err) => {
     console.log("O servidor esta rodando na porta 8080!");
 });
