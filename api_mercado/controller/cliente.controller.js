d0e0702520a8ac4d61db2fe8c77b05720506dffd
Leonardo <leopedrassani201@gmail.com>
2024-10-14 22:07:23 -0300

    Requisição de Cadastro

--- api_mercado/controller/cliente.controller.js
+++ api_mercado/controller/cliente.controller.js
@@ -2,14 +2,24 @@ const connection = require("../mysql-connection");
 
 module.exports = ({
     cadastro: (req, res) => { 
-        const {nome, telefone} = req.body;
+        const { nome, telefone } = req.body;
+    if (!nome) {
+        return res.status(400).send("Deve haver o campo nome");
+    } else if (!telefone) {
+        return res.status(400).send("Deve haver o campo telefone");
+    }
 
-        if(!nome){
-            return res.status(400).send("Deve haver o campo nome");
-        }
-        else if(!telefone){
-            return res.status(400).send("Deve haver o campo telefone");
-        }
+    connection.raw(`SELECT * FROM CLIENTE WHERE telefone = ${telefone}`)
+        .then((data) => {
+            if (data[0].length > 0) {
+                return res.status(400).send("Cliente já cadastrado");
+            } else {
+                 connection.raw(`INSERT INTO CLIENTE(nome, telefone) VALUES ("${nome}", "${telefone}");`);
+            }
+        }).catch((erro) => {
+            console.log(erro);
+            res.status(500).send("Erro ao cadastrar o cliente");
+        });
     },
     consultar: (req, res) => {
         connection.raw("SELECT * FROM CLIENTE").then((data) => {
