abb99b594553c252d5dd60fe8a8df2e5f75c67ab
Leonardo <leopedrassani201@gmail.com>
2024-10-30 22:08:41 -0300

    Cadastro de Pedido e Revisão do Inner Join

--- api_mercado/controller/pedido.controller.js
+++ api_mercado/controller/pedido.controller.js
@@ -1,17 +1,58 @@
 const conn = require("../mysql-connection");
-const { atualizar } = require("./produto.controller");
+
 
 module.exports = ({
-    cadastro : (req,res) =>{
-        
-    },
-    consulta: (req,res) =>{
+    cadastro : async (req,res) =>{
+        try{
+            const {id_cliente, id_produto, quantidade} = req.body;
 
+            if(!id_produto){
+                return res.status(400).send("O ID do produto esta faltando");
+            }else if(!id_cliente){
+                return res.status(400).send("O ID do Cliente esta faltando");
+            }
+            else if(!quantidade){
+                return res.status(400).send("Está faltando a quantidade do produto escolhido");
+            }
+
+            const produto = await conn.select("preco").from("produto").where({id: id_produto});
+            const total = quantidade * produto[0].preco;
+            const data = await conn("pedido").insert({id_cliente, id_produto, quantidade, total});
+
+            return res.status(200).send("O Pedido foi realizado com sucesso!");
+
+            
+        }catch(error){
+            console.log(error);
+            return res.status(500).send("Ocorreu um erro ao realizar o pedido");
+        };
     },
+
     atualizar: (req,res) =>{
 
     },
     deletar: (req,res) =>{
 
+    },
+    consulta: async (req,res) =>{
+        try{
+            const data = await conn.raw(`select
+                id_cliente as ID_do_Cliente,
+                cliente.nome as Nome,
+                cliente.telefone as Telefone,
+                id_produto as ID_do_Produto,
+                produto.nome as Nome_do_Produto,
+                produto.preco as Preco,
+                pedido.quantidade as Quantidade,
+                pedido.total as Total
+                from pedido
+                join cliente on pedido.id_cliente = cliente.id
+                join produto on pedido.id_produto = produto.id;`);
+
+                return res.send(data[0]);
+        }catch(error){
+            console.log(error);
+            return res.status(500).send("Ocorreu um erro ao realizar a Consulta");
+        }
     }
 });
--- api_mercado/routes/pedido.router.js
+++ api_mercado/routes/pedido.router.js
@@ -1,9 +1,10 @@
 const routes = require("express").Router();
+const pedidoController = require("../controller/pedido.controller");
 
 //CRUD
-routes.post("/", () => { });
-routes.get("/", () => { });
-routes.put("/:id([0-9]+)", () => { });
-routes.delete("/:id([0-9]+)", ()=>{});
+routes.post("/", pedidoController.cadastro);
+routes.get("/", pedidoController.consulta);
+routes.put("/:id([0-9]+)", pedidoController.atualizar);
+routes.delete("/:id([0-9]+)", pedidoController.deletar);
 
 module.exports = routes;
