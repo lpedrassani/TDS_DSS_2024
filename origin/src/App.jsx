6bee199011be97798b03b3d8d43d6798dfb93a58
Leonardo <leopedrassani201@gmail.com>
2024-11-11 21:49:18 -0300

    Primeira Aula de React

---
+++ origin/src/App.jsx
@@ -0,0 +1,12 @@
+import Teste from "./teste";
+
+function App(){
+  return(
+    <>
+      <h1>Ola mundo</h1>  
+      <Teste/>
+    </>
+  );
+}
+
+export default App
---
+++ origin/src/teste.jsx
@@ -0,0 +1,9 @@
+import "./Teste.css"
+
+function Teste(){
+    return (
+        <h2>Componente 2</h2>
+    )
+}
+
+export default Teste
